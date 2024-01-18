a295379b57c32a15741ca73de2e8adcccb13e4ba
phili <phili@PHIL-LAPTOP>
2024-01-18 12:16:17 +0000

    Add project files.

---
+++ BroCodeTutorial/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BroCodeTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
