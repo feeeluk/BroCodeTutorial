2df010cd29f3620da9f699660961df701ac87039
Phil Henning <philiphenning@outlook.com>
2024-01-25 21:42:04 +0000

    Lesson 48 - enums

--- BroCodeTutorial/Program.cs
+++ BroCodeTutorial/Program.cs
@@ -7,33 +7,56 @@ namespace BroCodeTutorial
     {
         static void Main(string[] args)
         {
-            // Lesson 47 - auto implemented properties = shortcut when no additional logic is required in the property
-            // you do not have to define a field for a property.
-            // you only have write get; and /or set; insdie the property
+            // Lesson 48 - enums = special 'class' that contains a set of named integer constants.
+            // Use enums when you have values that you know will not change.
+            // To get the integer value from an item, you must explicitly convert to an int.
+            // name = integer
 
-            Car car = new Car("Focus");
+            Console.WriteLine(Planets.Mercury);
+            Console.WriteLine((int)Planets.Earth);
 
-            Console.WriteLine(car.Model);
+            Console.WriteLine("#######################");
+
+            String name = PlanetsRadius.Earth.ToString();
+            int radius = (int)PlanetsRadius.Earth;
+            double volume = Volume(PlanetsRadius.Earth);
+
+            Console.WriteLine("Planet: " + name);
+            Console.WriteLine("Radius: " + radius);
+            Console.WriteLine("Volume: " + volume);
+
+        }
+
+        public static double Volume(PlanetsRadius radius)
+        {
+            double volume = (4.0/3.0) * Math.PI * Math.Pow((int)radius,3);
+            return volume;
         }
     }
 
-    class Car
+    enum Planets
     {
-        public String Model { get; set; }
-        
-        /*
-        String model;
+        Mercury,
+        Venus,
+        Earth,
+        Mars,
+        Juipiter,
+        Saturn,
+        Uranus,
+        Neptune,
+        Pluto
+    }
 
-        public String Model
-        {
-            get { return model; }
-            set { model = value; }
-        }
-        */
-
-        public Car(string model)        {
-            this.Model = model;
-        }
-
+    enum PlanetsRadius
+    {
+        Mercury = 2439,
+        Venus = 6051,
+        Earth = 6371,
+        Mars = 3389,
+        Juipiter = 69911,
+        Saturn = 58232,
+        Uranus = 25362,
+        Neptune = 24622,
+        Pluto = 1188
     }
 }
